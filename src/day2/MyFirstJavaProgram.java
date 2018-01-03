cfeaf714cdcb445354d16144ef71b2735ef1bcf9
AngryPineapple115 <league@WTS15.attlocal.net>
2018-01-03 14:55:54 -0800

    day_2

--- src/day2/MyFirstJavaProgram.java
+++ src/day2/MyFirstJavaProgram.java
@@ -8,8 +8,16 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot r2d2=new Robot();
+	r2d2.sparkle();
+	r2d2.setSpeed(3000);
+	r2d2.setPenColor(Color.BLUE);
+	r2d2.penDown();
 	
-		
-		
+	for(int i=0;i<100;i++) {
+	
+		r2d2.move(5);
+	r2d2.turn(360/100);
+	}
 	}
 }
